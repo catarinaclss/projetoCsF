d09b51804ed994d3d5ad4915de8507fb3857ca0e
Catarina <catarina.ufcg@gmail.com>
2014-09-14 20:14:10 -0500

    implementacao de teste para mudanca de tipo

--- test/GerenciadorDeViagensTest.java
+++ test/GerenciadorDeViagensTest.java
@@ -8,20 +8,23 @@ import models.ViagemLimitada;
 import models.ViagemPrivada;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class GerenciadorDeViagensTest {
 	
-	@Test
-	public void devePermitirAcessoParaLoginValido(){
-		
+	List<Usuario> participantes;
+	
+	@Before
+	public void GerenciadorDeViagens(){
+		participantes = new ArrayList<Usuario>();
 	}
+	
 	
 	@Test
 	public void deveExistirRestricoesParaViagem(){
 		ViagemAberta aberta = new ViagemAberta();
-		List<Usuario> participantes = new ArrayList<Usuario>();
 		Usuario u1 = null;
 		Usuario u2 = null;
 		Usuario u3 = null;
@@ -84,7 +87,6 @@ public class GerenciadorDeViagensTest {
 	@Test 
 	public void naoDevePermitirInsercoesRepetidas() throws Exception{
 		
-		List<Usuario> participantes_da_viagem = new ArrayList<Usuario>();
 		
 		Usuario u1 = null;
 		Usuario u2 = null;
@@ -106,16 +108,61 @@ public class GerenciadorDeViagensTest {
 		autorizados.add(u2);
 		privada.setAutorizados(autorizados);
 		
+
 		ViagemLimitada limitada = new ViagemLimitada();
 		limitada.setCodigo("0000");
 		
-		Assert.assertTrue(aberta.adicionarParticipante(participantes_da_viagem, null, u1));
-		Assert.assertFalse(aberta.adicionarParticipante(participantes_da_viagem, null, u1));
+		Assert.assertTrue(aberta.adicionarParticipante(participantes, null, u1));
+		Assert.assertFalse(aberta.adicionarParticipante(participantes, null, u1));
 	
-		Assert.assertTrue(privada.adicionarParticipante(participantes_da_viagem, null, u2));
-		Assert.assertFalse(privada.adicionarParticipante(participantes_da_viagem, null, u2));
+		Assert.assertTrue(privada.adicionarParticipante(participantes, null, u2));
+		Assert.assertFalse(privada.adicionarParticipante(participantes, null, u2));
 	
-		Assert.assertTrue(limitada.adicionarParticipante(participantes_da_viagem, "0000", u3));
-		Assert.assertFalse(limitada.adicionarParticipante(participantes_da_viagem, "0000", u3));
+		Assert.assertTrue(limitada.adicionarParticipante(participantes, "0000", u3));
+		Assert.assertFalse(limitada.adicionarParticipante(participantes, "0000", u3));
 	}
+	
+	@Test
+	public void permiteMudancasDeTipoDeViagem() throws Exception{
+		
+		Usuario u1 = null;
+		Usuario u2 = null;
+		Usuario u3 = null;
+		Usuario u4 = null;
+		Usuario u5 = null;
+		
+		try {
+			u1 = new Usuario("u1", "u1@", "123", new ArrayList<Viagem>());
+			u2 = new Usuario("u2", "u2@", "123", new ArrayList<Viagem>());
+			u3 = new Usuario("u3", "u3@", "123", new ArrayList<Viagem>());
+			u4 = new Usuario("u4", "u4@", "123", new ArrayList<Viagem>());
+			u5 = new Usuario("u5", "u5@", "123", new ArrayList<Viagem>());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Viagem viagem = new Viagem();
+		viagem.setEstrategia(new ViagemAberta());
+		
+		viagem.adicionarParticipante(null, u1);
+		Assert.assertTrue(viagem.adicionarParticipante(null, u2));
+		
+		viagem.setEstrategia(new ViagemLimitada());
+		viagem.mudarParaLimitada("000");
+		
+		Assert.assertTrue(viagem.adicionarParticipante("000", u3));
+		
+		List<Usuario> autorizados = new ArrayList<Usuario>();
+		autorizados.add(u4);
+		
+		viagem.mudarParaPrivada(autorizados);
+		//apesar de possuir o codigo correto, para quando a viagem era do tipo limitada
+		// ao realizar a mudanca, apenas usuarios autorizados podem participar da viagem
+		//respeitando a condicao do tipo especifico ViagemPrivada
+		Assert.assertFalse(viagem.adicionarParticipante("000", u5));
+		Assert.assertTrue(viagem.adicionarParticipante("", u4));
+		
+	}
+	
 }
